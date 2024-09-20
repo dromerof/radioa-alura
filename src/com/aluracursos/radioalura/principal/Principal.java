8855f5e480372cfd7d9965045b86b6b7227aef34
Daniel Romero <dandres508@gmail.com>
2024-09-19 19:11:38 -0500

    final del proyecto

--- src/com/aluracursos/radioalura/principal/Principal.java
+++ src/com/aluracursos/radioalura/principal/Principal.java
@@ -1,6 +1,7 @@
 package com.aluracursos.radioalura.principal;
 
 import com.aluracursos.radioalura.modelos.Cancion;
+import com.aluracursos.radioalura.modelos.Favoritos;
 import com.aluracursos.radioalura.modelos.Podcast;
 
 public class Principal {
@@ -13,6 +14,7 @@ public class Principal {
         miPodcast.setPresentador("Daniel Romero");
         miPodcast.setTitulo("Cafe.tech");
 
+        //Cancion
         for (int i = 0; i < 100; i++) {
             miCancion.meGusta();
         }
@@ -20,7 +22,19 @@ public class Principal {
             miCancion.reproduce();
         }
 
+        //PodCast
+        for (int i = 0; i < 180; i++) {
+            miPodcast.meGusta();
+        }
+        for (int i = 0; i < 9883; i++) {
+            miPodcast.reproduce();
+        }
+
         System.out.println("Total de reproducciones: " + miCancion.getTotalDeReproducciones());
         System.out.println("Total de me gustas: "+ miCancion.getTotalDeMeGusta());
+
+        Favoritos favoritos = new Favoritos();
+        favoritos.incluye(miPodcast);
+        favoritos.incluye(miCancion);
     }
 }
