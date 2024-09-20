5ca907c2ff7a7e30476e0207fe68361ce2543f8b
Daniel Romero <dandres508@gmail.com>
2024-09-19 19:03:33 -0500

    Intancias de canciones y podcast

---
+++ src/com/aluracursos/radioalura/principal/Principal.java
@@ -0,0 +1,26 @@
+package com.aluracursos.radioalura.principal;
+
+import com.aluracursos.radioalura.modelos.Cancion;
+import com.aluracursos.radioalura.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Cancion miCancion = new Cancion();
+        miCancion.setTitulo("Forever");
+        miCancion.setCantante("kiss");
+
+        Podcast miPodcast = new Podcast();
+        miPodcast.setPresentador("Daniel Romero");
+        miPodcast.setTitulo("Cafe.tech");
+
+        for (int i = 0; i < 100; i++) {
+            miCancion.meGusta();
+        }
+        for (int i = 0; i < 5000; i++) {
+            miCancion.reproduce();
+        }
+
+        System.out.println("Total de reproducciones: " + miCancion.getTotalDeReproducciones());
+        System.out.println("Total de me gustas: "+ miCancion.getTotalDeMeGusta());
+    }
+}
